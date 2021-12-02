472b2698a462adda583256be6fc84d0f622937d6
Tan Wang Leng <tanwangleng@outlook.com>
2021-12-02 13:27:19 +0800

    Clean up day 02

--- src/bin/d02/main.rs
+++ src/bin/d02/main.rs
@@ -1,70 +1,105 @@
 const ACTUAL_INPUT: &str = include_str!("input.txt");
 
-fn p1(input: &str) -> String {
-    let result = input.trim().lines().fold((0, 0), |acc, instruction| {
-        let parts = instruction.split(' ').collect::<Vec<_>>();
-
-        if parts.len() != 2 {
-            panic!("Expected 2 parts, found {}", instruction);
-        }
-
-        let count = parts[1].parse::<i32>().unwrap();
-
-        match parts[0] {
-            "forward" => (acc.0 + count, acc.1),
-            "down" => (acc.0, acc.1 + count),
-            "up" => (acc.0, acc.1 - count),
-            _ => panic!("Expected action, found {}", parts[0]),
-        }
-    });
-
-    (result.0 * result.1).to_string()
+enum Instruction {
+    Forward(i32),
+    Down(i32),
+    Up(i32),
 }
 
+impl Instruction {
+    fn parse(line: &str) -> Self {
+        let parts = line.split(' ').collect::<Vec<_>>();
+
+        if parts.len() != 2 {
+            panic!("Expected 2 parts, found '{}'", line);
+        }
+
+        let count = parts[1]
+            .parse::<i32>()
+            .unwrap_or_else(|_| panic!("Expect value for second part, found '{}'", line));
+
+        match parts[0] {
+            "forward" => Instruction::Forward(count),
+            "down" => Instruction::Down(count),
+            "up" => Instruction::Up(count),
+            _ => panic!("Expected action for first part, found '{}'", line),
+        }
+    }
+}
+
+#[derive(Default)]
 struct Submarine {
     horizontal: i32,
     depth: i32,
     aim: i32,
 }
 
+impl Submarine {
+    fn p1_do(&self, instruction: &Instruction) -> Self {
+        match *instruction {
+            Instruction::Forward(x) => Submarine {
+                horizontal: self.horizontal + x,
+                depth: self.depth,
+                aim: self.aim,
+            },
+            Instruction::Down(x) => Submarine {
+                horizontal: self.horizontal,
+                depth: self.depth + x,
+                aim: self.aim,
+            },
+            Instruction::Up(x) => Submarine {
+                horizontal: self.horizontal,
+                depth: self.depth - x,
+                aim: self.aim,
+            },
+        }
+    }
+
+    fn p2_do(&self, instruction: &Instruction) -> Self {
+        match *instruction {
+            Instruction::Forward(x) => Submarine {
+                horizontal: self.horizontal + x,
+                depth: self.depth + self.aim * x,
+                aim: self.aim,
+            },
+            Instruction::Down(x) => Submarine {
+                horizontal: self.horizontal,
+                depth: self.depth,
+                aim: self.aim + x,
+            },
+            Instruction::Up(x) => Submarine {
+                horizontal: self.horizontal,
+                depth: self.depth,
+                aim: self.aim - x,
+            },
+        }
+    }
+
+    fn get_answer(&self) -> String {
+        (self.horizontal * self.depth).to_string()
+    }
+}
+
+fn p1(input: &str) -> String {
+    input
+        .trim()
+        .lines()
+        .map(Instruction::parse)
+        .fold(Submarine::default(), |acc, instruction| {
+            acc.p1_do(&instruction)
+        })
+        .get_answer()
+}
+
 fn p2(input: &str) -> String {
-    let result = input.trim().lines().fold(
-        Submarine {
-            horizontal: 0,
-            depth: 0,
-            aim: 0,
-        },
-        |acc, instruction| {
-            let parts = instruction.split(' ').collect::<Vec<_>>();
-
-            if parts.len() != 2 {
-                panic!("Expected 2 parts, found {}", instruction);
-            }
-
-            let count = parts[1].parse::<i32>().unwrap();
-
-            match parts[0] {
-                "forward" => Submarine {
-                    horizontal: acc.horizontal + count,
-                    depth: acc.depth + acc.aim * count,
-                    aim: acc.aim,
-                },
-                "down" => Submarine {
-                    horizontal: acc.horizontal,
-                    depth: acc.depth,
-                    aim: acc.aim + count,
-                },
-                "up" => Submarine {
-                    horizontal: acc.horizontal,
-                    depth: acc.depth,
-                    aim: acc.aim - count,
-                },
-                _ => panic!("Expected action, found {}", parts[0]),
-            }
-        },
-    );
-
-    (result.horizontal * result.depth).to_string()
+    input
+        .trim()
+        .lines()
+        .map(Instruction::parse)
+        .fold(Submarine::default(), |acc, instruction| {
+            acc.p2_do(&instruction)
+        })
+        .get_answer()
 }
 
 fn main() {
