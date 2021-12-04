0c00528981194e06b48ac89e695bc6dc80dd26c3
Tan Wang Leng <tanwangleng@outlook.com>
2021-12-04 14:43:00 +0800

    Improve day 4

--- src/bin/d04/main.rs
+++ src/bin/d04/main.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Ordering, collections::HashMap, iter};
+use std::{collections::HashMap, iter};
 
 const ACTUAL_INPUT: &str = include_str!("input.txt");
 const BOARD_SIZE: i32 = 5;
@@ -14,13 +14,10 @@ struct Board {
     coords: HashMap<i32, (i32, i32)>,
 }
 
-enum BoardCanWin {
-    Winnable {
-        called_index: i32,
-        called_number: i32,
-        unmarked_numbers_sum: i32,
-    },
-    NotWinnable,
+struct BoardWinState {
+    called_index: i32,
+    called_number: i32,
+    unmarked_numbers_sum: i32,
 }
 
 impl Board {
@@ -29,33 +26,32 @@ impl Board {
             panic!("Expected 5x5 board, found {} rows", lines.len());
         }
 
-        let numbers = lines
+        let mut coords = HashMap::new();
+
+        lines
             .into_iter()
             .map(|line| {
                 line.split(' ')
-                    .filter(|x| !x.is_empty())
+                    .filter(|x| !x.is_empty()) // input contains multiple spaces, these will show up as "" unless we remove them
                     .map(str::parse::<i32>)
                     .map(Result::unwrap)
                     .collect::<Vec<_>>()
             })
-            .collect::<Vec<_>>();
+            .enumerate()
+            .for_each(|(y, rows)| {
+                if rows.len() != BOARD_SIZE as usize {
+                    panic!("Expected 5x5 board, found {} cols in row {}", rows.len(), y);
+                }
 
-        let mut coords = HashMap::new();
-
-        numbers.into_iter().enumerate().for_each(|(y, rows)| {
-            if rows.len() != BOARD_SIZE as usize {
-                panic!("Expected 5x5 board, found {} cols in row {}", rows.len(), y);
-            }
-
-            rows.into_iter().enumerate().for_each(|(x, number)| {
-                coords.insert(number, (y as i32, x as i32));
+                rows.into_iter().enumerate().for_each(|(x, number)| {
+                    coords.insert(number, (y as i32, x as i32));
+                });
             });
-        });
 
         Self { coords }
     }
 
-    fn is_winnable(&self, called_numbers: &Vec<i32>) -> BoardCanWin {
+    fn get_win_state(&self, called_numbers: &[i32]) -> Option<BoardWinState> {
         let mut rows_remaining = iter::repeat(BOARD_SIZE)
             .take(BOARD_SIZE as usize)
             .collect::<Vec<_>>();
@@ -65,145 +61,105 @@ impl Board {
 
         let mut remaining_numbers = self.coords.clone();
 
-        for (index, number) in called_numbers.iter().enumerate() {
-            let number_coord = remaining_numbers.remove(number);
+        called_numbers
+            .iter()
+            .enumerate()
+            .find_map(|(index, number)| match remaining_numbers.remove(number) {
+                None => None,
+                Some(number_coord) => {
+                    {
+                        let y = number_coord.0 as usize;
+                        rows_remaining[y] -= 1;
 
-            if let Some(number_coord) = number_coord {
-                {
-                    let y = number_coord.0 as usize;
-                    rows_remaining[y] -= 1;
-
-                    if rows_remaining[y] == 0 {
-                        return BoardCanWin::Winnable {
-                            called_index: index as i32,
-                            called_number: *number,
-                            unmarked_numbers_sum: remaining_numbers.keys().into_iter().sum(),
-                        };
+                        if rows_remaining[y] == 0 {
+                            return Some(BoardWinState {
+                                called_index: index as i32,
+                                called_number: *number,
+                                unmarked_numbers_sum: remaining_numbers.keys().into_iter().sum(),
+                            });
+                        }
                     }
-                }
 
-                {
-                    let x = number_coord.1 as usize;
-                    cols_remaining[x] -= 1;
-                    if cols_remaining[x] == 0 {
-                        return BoardCanWin::Winnable {
-                            called_index: index as i32,
-                            called_number: *number,
-                            unmarked_numbers_sum: remaining_numbers.keys().into_iter().sum(),
-                        };
+                    {
+                        let x = number_coord.1 as usize;
+                        cols_remaining[x] -= 1;
+                        if cols_remaining[x] == 0 {
+                            return Some(BoardWinState {
+                                called_index: index as i32,
+                                called_number: *number,
+                                unmarked_numbers_sum: remaining_numbers.keys().into_iter().sum(),
+                            });
+                        }
                     }
+
+                    None
                 }
+            })
+    }
+}
+
+struct Input {
+    called_numbers: Vec<i32>,
+    boards: Vec<Board>,
+}
+
+impl Input {
+    fn parse_input(input: &str) -> Self {
+        let mut lines = input.trim().lines();
+
+        let called_numbers = parse_called_numbers(lines.next().unwrap());
+
+        let mut boards = vec![];
+
+        loop {
+            lines.next();
+
+            let current_board = (0..BOARD_SIZE)
+                .map(|_| lines.next())
+                .flatten()
+                .collect::<Vec<_>>();
+
+            if current_board.is_empty() {
+                // we finish parsing
+                break;
             }
+
+            boards.push(Board::parse_from_lines(current_board));
         }
 
-        BoardCanWin::NotWinnable
+        Self {
+            called_numbers,
+            boards,
+        }
     }
 }
 
 fn p1(input: &str) -> String {
-    let mut lines = input.trim().lines();
+    let input = Input::parse_input(input);
 
-    let called_numbers = parse_called_numbers(lines.next().unwrap());
-
-    let mut boards = vec![];
-
-    loop {
-        lines.next();
-
-        let current_board = (0..BOARD_SIZE)
-            .map(|_| lines.next())
-            .flatten()
-            .collect::<Vec<_>>();
-
-        if current_board.is_empty() {
-            // we finish parsing
-            break;
-        }
-
-        boards.push(Board::parse_from_lines(current_board));
-    }
-
-    let result = boards
+    let result = input
+        .boards
         .into_iter()
-        .map(|board| board.is_winnable(&called_numbers))
-        .min_by(|x, y| match x {
-            BoardCanWin::NotWinnable => Ordering::Greater,
-            BoardCanWin::Winnable {
-                called_index: x_index,
-                ..
-            } => match y {
-                BoardCanWin::NotWinnable => Ordering::Less,
-                BoardCanWin::Winnable {
-                    called_index: y_index,
-                    ..
-                } => x_index.cmp(y_index),
-            },
-        })
-        .unwrap();
+        .map(|board| board.get_win_state(&input.called_numbers))
+        .flatten()
+        .min_by(|x, y| x.called_index.cmp(&y.called_index))
+        .expect("Cannot find a winning board");
 
-    if let BoardCanWin::Winnable {
-        called_number,
-        unmarked_numbers_sum,
-        ..
-    } = result
-    {
-        (called_number * unmarked_numbers_sum).to_string()
-    } else {
-        panic!("Cannot find a winning board");
-    }
+    (result.called_number * result.unmarked_numbers_sum).to_string()
 }
 
 fn p2(input: &str) -> String {
-    let mut lines = input.trim().lines();
+    let input = Input::parse_input(input);
 
-    let called_numbers = parse_called_numbers(lines.next().unwrap());
-
-    let mut boards = vec![];
-
-    loop {
-        lines.next();
-
-        let current_board = (0..BOARD_SIZE)
-            .map(|_| lines.next())
-            .flatten()
-            .collect::<Vec<_>>();
-
-        if current_board.is_empty() {
-            // we finish parsing
-            break;
-        }
-
-        boards.push(Board::parse_from_lines(current_board));
-    }
-
-    let result = boards
+    let result = input
+        .boards
         .into_iter()
-        .map(|board| board.is_winnable(&called_numbers))
-        .max_by(|x, y| match x {
-            BoardCanWin::NotWinnable => Ordering::Less,
-            BoardCanWin::Winnable {
-                called_index: x_index,
-                ..
-            } => match y {
-                BoardCanWin::NotWinnable => Ordering::Greater,
-                BoardCanWin::Winnable {
-                    called_index: y_index,
-                    ..
-                } => x_index.cmp(y_index),
-            },
-        })
-        .unwrap();
+        .map(|board| board.get_win_state(&input.called_numbers))
+        .flatten()
+        .max_by(|x, y| x.called_index.cmp(&y.called_index))
+        .expect("Cannot find a last losing board");
 
-    if let BoardCanWin::Winnable {
-        called_number,
-        unmarked_numbers_sum,
-        ..
-    } = result
-    {
-        (called_number * unmarked_numbers_sum).to_string()
-    } else {
-        panic!("Cannot find a last-losing board");
-    }
+    (result.called_number * result.unmarked_numbers_sum).to_string()
 }
 
 fn main() {
